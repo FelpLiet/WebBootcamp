dc1ed847c541e8b1828d20d7d32ccfcdcf590f62
Felipe Nogueira <felipe.leite23@gmail.com>
2024-06-12 12:23:17 -0300

    fix do get de ID com UUID e uma /

--- restDemo/index.js
+++ restDemo/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
+uuid();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -9,22 +11,22 @@ app.use(express.urlencoded({ extended: true }));
 
 let comments = [
   {
-    id: 0,
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
-    id: 1,
+    id: uuid,
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    id: 2,
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
-    id: 3,
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
@@ -36,14 +38,21 @@ app.get("/comments", (req, res) => {
 
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ id: uuid(), username, comment });
   res.redirect("/comments");
 });
 
 app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
-  const comment = comments.find((c) => c.id === parseInt(id));
-  res.render("comments/show", { comment });
+  console.log(req.params)
+  const comment = comments.find((c) => c.id === id);
+  console.log("Comment ID:", id);
+  console.log("Found comment:", comment);
+  if(comment){
+    res.render("comments/show", { comment });
+  } else {
+    res.status(404).send("ID Not Found");
+  }
 });
 
 app.listen(3000, () => {
