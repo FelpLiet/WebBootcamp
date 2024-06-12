d19ef0479b1e25a4675e6a5abe9bf97858258a15
Felipe Nogueira <felipe.leite23@gmail.com>
2024-06-12 19:18:16 -0300

    edit and delete comments

--- restDemo/index.js
+++ restDemo/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid");
 uuid();
 
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -16,7 +18,7 @@ let comments = [
     comment: "lol that is so funny!",
   },
   {
-    id: uuid,
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
@@ -53,6 +55,34 @@ app.get("/comments/:id", (req, res) => {
   } else {
     res.status(404).send("ID Not Found");
   }
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  if(foundComment){
+    foundComment.comment = newCommentText;
+    res.redirect("/comments");
+  } else {
+    res.status(404).send("ID Not Found");
+  }
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  if(comment){
+    res.render("comments/edit", { comment });
+  } else {
+    res.status(404).send("ID Not Found");
+  }
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
