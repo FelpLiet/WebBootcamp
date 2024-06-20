fb18705f498fb228a51702b8622f6af9b731ae06
Felipe Nogueira <felipe.leite23@gmail.com>
2024-06-20 20:46:15 -0300

    adding and updating with mongoose

--- mongooseEspress/index.js
+++ mongooseEspress/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Product = require('./models/product');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
     console.log('Failed to connect to MongoDB', err);
   });
 
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -24,11 +26,33 @@ app.get('/products', async (req, res) => {
   res.render('products/index', { products });
 });
 
+app.get('/products/new', (req, res) => {
+  res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+});
+
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   console.log(product)
   res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
--- mongooseEspress/public/script.js
+++ mongooseEspress/public/script.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".clickable-li").forEach(item => {
-    item.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = ["#editButton", "#newButton"];
+  document.querySelectorAll(".clickable-li").forEach((item) => {
+    item.addEventListener("click", function () {
       window.location.href = this.getAttribute("data-url");
     });
   });
+
+  function getUrl(elements) {
+    for (let element of elements) {
+      console.log(element);
+      if (document.querySelector(element) === null) {
+        continue;
+      } else {
+        document.querySelector(element).addEventListener("click", function () {
+          window.location.href = this.getAttribute("data-url");
+        });
+      }
+    }
+  }
+
+  getUrl(elements);
 });
